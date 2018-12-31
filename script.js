2c5db719d1f39d02edc4541c4e18e98e2559c53d
Guilherme A. Melo <guilhermearmelo@gmail.com>
2018-12-31 11:14:10 -0200

    Site Larissa

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body =  document.getElementById("gradient");
-
-body.style.background = "red";
-
-function setGradient() {
-    body.style.background = 
-    "linear-gradient(to right, " 
-    + color1.value 
-    + ", "
-    + color2.value 
-    + ")";
-
-    css.textContent = body.style.background + ";";
-}
-
-color1.addEventListener("input", setGradient);
-
-color2.addEventListener("input", setGradient);
+$(document).ready(function(){
+    // Add smooth scrolling to all links in navbar + footer link
+    $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 900, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+    
+    $(window).scroll(function() {
+      $(".slideanim").each(function(){
+        var pos = $(this).offset().top;
+  
+        var winTop = $(window).scrollTop();
+          if (pos < winTop + 600) {
+            $(this).addClass("slide");
+          }
+      });
+    });
+  })
